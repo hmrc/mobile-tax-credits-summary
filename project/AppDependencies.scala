10ebb6cba4723ecccc179e286e49365165379b8c
aerodigi <54714520+aerodigi@users.noreply.github.com>
2022-06-14 11:05:31 +0100

    [HMA-5817] Update service dependency versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ private object AppDependencies {
 
   private val bootstrapPlayVersion = "5.16.0"
   private val playHmrcApiVersion   = "6.4.0-play-28"
-  private val domainVersion        = "6.2.0-play-28"
+  private val domainVersion        = "8.1.0-play-28"
 
   private val scalaTestVersion         = "3.0.8"
   private val scalaTestPlusPlayVersion = "4.0.3"
