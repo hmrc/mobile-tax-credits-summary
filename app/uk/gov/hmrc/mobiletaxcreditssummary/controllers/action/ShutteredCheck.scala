58bd1532fbf17e1689dbc9d32a7eaad40bd94e22
pdhiman3006 <176318726+pdhiman3006@users.noreply.github.com>
2025-04-07 15:17:12 +0100

    HMA-9897 change shuttering response

--- app/uk/gov/hmrc/mobiletaxcreditssummary/controllers/action/ShutteredCheck.scala
+++ app/uk/gov/hmrc/mobiletaxcreditssummary/controllers/action/ShutteredCheck.scala
@@ -27,6 +27,6 @@ trait ShutteredCheck extends Results {
   private final val WebServerIsDown = new Status(521)
 
   def withShuttering(shuttering: Shuttering)(fn: => Future[Result]): Future[Result] =
-    if (shuttering.shuttered) Future.successful(WebServerIsDown(Json.toJson(shuttering))) else fn
+    if (shuttering.shuttered) Future.successful(Ok("")) else fn
 
 }
